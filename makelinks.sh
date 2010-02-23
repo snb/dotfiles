cc750d398199484e8cbb801ffed43af3d5263d67
Nick Barkas <snb@spotify.com>
2010-02-23 17:29:38 +0100

    Make symlinks. Get rid of .pydistutils.cfg

--- makelinks.sh
+++ makelinks.sh
@@ -3,3 +3,11 @@
 
 # XXX this should create symlinks in $HOME to the appropriate files in this
 # directory.
+
+DOTFILES=".gitconfig .muttrc .vim .vimrc .zshrc"
+
+for file in $DOTFILES; do
+    ln -s .dotfiles/$file .
+done
+
+ln -s .dotfiles/.ssh-config .ssh/config
