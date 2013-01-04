02bb12261766e5759ed00efb11d2b97adf1f89c6
Nick Barkas <nick.barkas@gmail.com>
2013-01-03 21:05:13 -0500

    add global gitignore file

--- makelinks.sh
+++ makelinks.sh
@@ -3,7 +3,7 @@
 
 # Create symlinks in $HOME to the appropriate files in this directory.
 
-DOTFILES=".gitconfig .muttrc .vim .vimrc .zshrc"
+DOTFILES=".gitconfig .muttrc .vim .vimrc .zshrc .gitignore_global"
 
 for file in $DOTFILES; do
     ln -s .dotfiles/$file .
